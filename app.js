d3bd863e715e66a83dbb93e5e1e3531156e7ffb2
Johnny Lay <johnnylay1@hotmail.com>
2022-07-13 14:57:16 +1000

    ternary operator

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Strings and Number 
 
-var str = 'Johnny'
-console.log(str[6 - 1] )
+// var str = 'Johnny'
+// console.log(str[6 - 1] )
 
 let temperature = 20
 let isRaining = true
@@ -24,33 +24,65 @@ console.log(bool)
 
 // Conditionals
 
+// let subscribed = false
+// let loggedIn = true
+
+// if (subscribed === true) {
+//     console.log('show the video')
+// }
+
+// else if (loggedIn === true) {
+//     console.log('tell the user to upgrade their subscription')
+// }
+
+// else {
+//     console.log('tell user to log into account')
+// }
+
+// let cash = 10
+// let price = 50
+// let difference = cash - price
+
+// if (cash > price) {
+//     console.log(`you paid extra - here's ${difference} dollars change`)
+// }
+
+// else if (cash === price) {
+//     console.log("you paid the exact amount, have a nice day!")
+// }
+
+// else {
+//     console.log(`not enough money - you still owe ${difference * -1} dollars`)
+// }
+
+let cash = 50
+let price = 40
+let isStoreOpen = false
+
+if (cash >= price && isStoreOpen === true) {
+    console.log('print the reciept')
+}
+
+// truthy or falsy values
+
+let val = "0";
+
+if (val) {
+    console.log(!!val)
+}
+
+else {
+    console.log(!!val)
+}
+
+// Ternary Operators
+
+let hot = true
+
+hot ? console.log('weather is hot outside') : console.log('weatehr is cold')
+
 let subscribed = false
 let loggedIn = true
 
-if (subscribed === true) {
-    console.log('show the video')
-}
-
-else if (loggedIn === true) {
-    console.log('tell the user to upgrade their subscription')
-}
-
-else {
-    console.log('tell user to log into account')
-}
-
-let cash = 10
-let price = 50
-let difference = cash - price
-
-if (cash > price) {
-    console.log(`you paid extra - here's ${difference} dollars change`)
-}
-
-else if (cash === price) {
-    console.log("you paid the exact amount, have a nice day!")
-}
-
-else {
-    console.log(`not enough money - you still owe ${difference * -1} dollars`)
-}
+let str = subscribed && loggedIn ? 'show the video' : 'hide the video'
+console.log(str)
